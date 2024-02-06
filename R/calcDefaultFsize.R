f42e1a26a4f837fa1e3d409d38c2e2b8d65d76d7
Hanxi-002 <xiaoh@pitt.edu>
2024-02-06 11:28:22 -0500

    added print statement in calcFsize

--- R/calcDefaultFsize.R
+++ R/calcDefaultFsize.R
@@ -7,7 +7,11 @@
 
 calcDefaultFsize <- function(y, all_latent_factors){
   
+  cat(dim(y[[1]]))
+  cat(all_latent_factors$K)
+  
   if ((dim(y)[[1]] <= all_latent_factors$K) && (all_latent_factors$K < 100)){
+    cat("In calcDefaultFsize block 1")
     if (abs(dim(y)[[1]]-all_latent_factors$K) <= 2){
       f_size = dim(y)[[1]] - 2
     } else{
@@ -16,11 +20,14 @@ calcDefaultFsize <- function(y, all_latent_factors){
   }
   
   if ((dim(y)[[1]] > all_latent_factors$K) && (all_latent_factors$K < 100)){
+    cat("In calcDefaultFsize block 2")
     f_size = all_latent_factors$K
   }
 
   if (all_latent_factors$K >= 100){
+    cat("In calcDefaultFsize block 3")
     f_size = 100
   }
 return(f_size)
 }
+
