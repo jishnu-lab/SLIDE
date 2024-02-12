c95c1621a3a178f12d79d9c1bfedc9730cc2f2be
jrahimik <jrahimik1986@gmail.com>
2024-02-12 18:52:27 -0500

    updating zs

--- R/calcDefaultFsize.R
+++ R/calcDefaultFsize.R
@@ -6,8 +6,8 @@
 
 
 calcDefaultFsize <- function(y, all_latent_factors){
-  
-  
+
+
   if ((dim(y)[[1]] <= all_latent_factors$K) && (all_latent_factors$K < 100)){
     if (abs(dim(y)[[1]]-all_latent_factors$K) <= 2){
       f_size = dim(y)[[1]] - 2
@@ -15,13 +15,13 @@ calcDefaultFsize <- function(y, all_latent_factors){
       f_size = dim(y)[[1]]
     }
   }
-  
+
   if ((dim(y)[[1]] > all_latent_factors$K) && (all_latent_factors$K < 100)){
     f_size = all_latent_factors$K
   }
 
-  if (all_latent_factors$K >= 100){
-    f_size = 100
+  if (dim(y)[[1]] < all_latent_factors$K){
+    f_size = dim(y)[[1]]
   }
 return(f_size)
 }
