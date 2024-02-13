d33af7a5f0195339235f9c6cd61edc913661b586
Hanxi-002 <xiaoh@pitt.edu>
2024-02-13 10:51:36 -0500

    updated zeroFiltering

--- R/zeroFiltering.R
+++ R/zeroFiltering.R
@@ -1,24 +1,48 @@
 #' Reduce sparsity of the data matrix by deleting samples and features that are expressed in few features and samples. 
 #'
-#' @param data the data matrix
-#' @param g_thresh samples with number of zeros greater then g_thresh will be filtered out
-#' @param c_thresh features with number of zeros greater then g_thresh will be filtered out
+#' @param yaml_path the path to the yaml file
+#' @param g_thresh features with number of zeros greater then g_thresh will be filtered out
+#' @param c_thresh samples with number of zeros greater than c_thresh will be filtered out
 #' @export
 
 
-zeroFiltering <- function(data, g_thresh, c_thresh){
-  cat("Original dataframe dimension is ", dim(data)[[1]], " by ", dim(data)[[2]], "\n")
+zeroFiltering <- function(yaml_path, g_thresh, c_thresh){
+  input_params <- yaml::yaml.load_file(yaml_path)
+  
+  x <- as.matrix(utils::read.csv(input_params$x_path, row.names = 1))
+  y <- as.matrix(utils::read.csv(input_params$y_path, row.names = 1))
+  cat("Original dataframe dimension is ", dim(x)[[1]], " by ", dim(x)[[2]], "\n")
+  
+  if (sum(row.names(x) == row.names(y)) != nrow(x)) {stop('Input data matrix and response does not have matching row names.')}
   
   #filtered out cells with num zeros greater than a certain threshold
-  thresh <- dim(data)[[2]] - g_thresh
-  i <- rowSums(data == 0, na.rm=TRUE) <= thresh
-  filtered <- data[i, ]
+  #thresh <- dim(x)[[2]] - c_thresh
+  i <- rowSums(x == 0, na.rm=TRUE) <= c_thresh
+  filtered <- x[i, ]
   
   #filtered out genes with num zeros greater than a certain threshold
-  thresh <- dim(data)[[1]] - c_thresh
-  i <- colSums(data == 0, na.rm=TRUE) <= thresh
+  #thresh <- dim(data)[[1]] - g_thresh
+  i <- colSums(x == 0, na.rm=TRUE) <= g_thresh
   filtered <- filtered[ ,i]
   
   cat("Filtered dataframe dimension is ", nrow(filtered), " by ", ncol(filtered), "\n")
-  return(filtered)
+  
+  ### make sure Y has the same names
+  filtered_y <- as.matrix(y[row.names(y) %in% row.names(filtered), ])
+  colnames(filtered_y) = colnames(y)
+  if (nrow(filtered_y) != nrow(filtered)) {stop("Number of samples in data matrix and response do not match post-filtering.")}
+  
+  
+  # check if output path exists
+  if (dir.exists(input_params$out_path)){
+    cat("Populating filtered files to ", input_params$out_path, '.\n')
+  } else{
+    cat ("Output folder not found, creating at ", input_params$out_path, ".\n")
+    dir.create(file.path(input_params$out_path), showWarnings = F, recursive = T)
+  }
+  
+  write.csv(filtered, paste0(input_params$out_path, "/filtered_x.csv"))
+  write.csv(filtered_y, paste0(input_params$out_path, "/filtered_y.csv"))
 }
+
+
