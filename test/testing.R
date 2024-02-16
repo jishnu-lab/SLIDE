ac4a56e3116a276f02abe1c641efa70246a5c93d
Hanxi-002 <xiaoh@pitt.edu>
2024-02-16 15:05:16 -0500

    delete main.R

--- test/testing.R
+++ test/testing.R
@@ -3,4 +3,4 @@ checkDataParams("/ix/djishnu/Hanxi/SLIDE/test/test.yaml")
 
 yaml_path = "/ix/djishnu/Hanxi/test/SLIDE_main_s3Vs2Day28.yaml"
 input_params <- yaml::yaml.load_file(yaml_path)
-Optimize_SLIDE(input_params, sink_file = FALSE)
+optimizeSLIDE(input_params, sink_file = FALSE)
