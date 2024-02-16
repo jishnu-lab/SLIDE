bdffc77314c173d061ff381175de0db9c09bb167
Hanxi-002 <xiaoh@pitt.edu>
2024-02-16 15:01:36 -0500

    delete main.R

--- test/testing.R
+++ test/testing.R
@@ -1,10 +1,3 @@
-library(doParallel)
-cores <-  as.numeric(Sys.getenv('SLURM_CPUS_PER_TASK', unset=NA))
-if(is.na(cores)) cores <- detectCores()
-# cores <- 6
-registerDoParallel(cores)
-cat('number of cores using', cores, '. . .\n')
-
 checkDataParams("/ix/djishnu/Hanxi/SLIDE/test/test.yaml")
 #zeroFiltering(yaml_path = "/ix/djishnu/Hanxi/SLIDE/test/test.yaml", 24, 804)
 
