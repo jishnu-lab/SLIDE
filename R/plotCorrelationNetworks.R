6a94783ad5e89e5ecb1cab478ec90554c7247c9f
Transcription Factory <transcriptionfactory@gmail.com>
2024-02-24 14:53:19 -0500

    Adding to SLIDE vignette and updating examples with correlation plots (#18)
    * fixing vignette error; adding plotCorrelationNetworks
    
    * working directory issue for plotCorrelationNetworks
    
    * fixed vignette pdf; plotCorrelationNetworks having some path issue might related to qgraph plotting in working directory (and trying to change working directory to get around this)
    
    * changing working directory back to original directory in each loop to avoid issues with relative paths
    
    * fixed plot colors and correlations
    
    * rounding correlations to integers so getting weird colors
    
    * updated vignette with summary table and correlation network function call
    
    rebuilt vignette from scratch and reran examples without error

--- R/plotCorrelationNetworks.R
+++ R/plotCorrelationNetworks.R
@@ -32,13 +32,21 @@ plotCorrelationNetworks = function(input_params) {
       cat("\n No feature lists found. Run optimizeSLIDE first \n")
     }
 
+<<<<<<< HEAD
+=======
     setwd(dir_name)
+>>>>>>> main
 
     for (f in gene_list_files) {
 
       LF_num = unlist(stringr::str_match(f, pattern = "Z[0-9]+"))
       temp_list = read.table(f, header = TRUE) %>% tidyr::drop_na()
 
+<<<<<<< HEAD
+      setwd(dir_name)
+
+=======
+>>>>>>> main
       # subset genes from expression data
       x_gene = as.matrix(x[, temp_list$names])
 
@@ -46,11 +54,19 @@ plotCorrelationNetworks = function(input_params) {
       if (length(unique(y)) == 2) {
         col_auc = round(apply(x_gene, 2, function(xs) glmnet:::auc(as.matrix(y), as.matrix(xs))), 2)
 
+<<<<<<< HEAD
+        temp_cols = ifelse(col_auc > 0.5, "salmon", ifelse(col_auc < 0.5, "skyblue", "lightgray"))
+      } else {
+        col_cor = round(apply(x_gene, 2, function(xs) cor(as.matrix(y), as.matrix(xs), method = "spearman")), 2)
+
+        temp_cols = ifelse(col_cor > 0, "salmon", ifelse(col_cor < 0, "skyblue", "lightgray"))
+=======
         temp_cols = ifelse(col_auc > 0.55, "salmon", ifelse(col_auc < 0.45, "skyblue", "lightgray"))
       } else {
         col_cor = round(apply(x_gene, 2, function(xs) glmnet:::auc(as.matrix(y), as.matrix(xs))), 2)
 
         temp_cols = ifelse(col_cor > 0.1, "salmon", ifelse(col_cor < -0.1, "skyblue", "lightgray"))
+>>>>>>> main
       }
 
       x_temp = cor(x_gene)
@@ -61,7 +77,14 @@ plotCorrelationNetworks = function(input_params) {
                           title = LF_num,
                           label.scale.equal=FALSE,label.prop=0.95,shape="ellipse",
                           posCol="#40006D", negCol="#59A14F",filetype='pdf',height=5,width=7)
+<<<<<<< HEAD
+      setwd(original_wd)
+
+    }
+  }
+=======
     }
   }
   setwd(original_wd)
+>>>>>>> main
 }
