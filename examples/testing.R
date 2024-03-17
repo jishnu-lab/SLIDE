55cc57aa0914996f697abcf852869e29e2b7fd60
Transcription Factory <transcriptionfactory@gmail.com>
2024-03-17 15:31:59 -0400

    Update testing.R

--- examples/testing.R
+++ examples/testing.R
@@ -1,5 +1,7 @@
 yaml_path = "/ix/djishnu/Hanxi/SLIDE/examples/example_continuous.yaml"
 input_params <- yaml::yaml.load_file(yaml_path)
 
-checkDataParams(input_params)
-optimizeSLIDE(input_params, sink_file = FALSE)
+SLIDE::checkDataParams(input_params)
+SLIDE::optimizeSLIDE(input_params, sink_file = FALSE)
+SLIDE::plotCorrelationNetworks(input_params)
+SLIDE::SLIDEcv(yaml_path, nrep = 20, k = 5)
