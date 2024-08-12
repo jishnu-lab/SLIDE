c01bfdc78d4c56183dcae5cf58eab3bc50bfd8a6
jrahimik <jrahimik1986@gmail.com>
2024-08-12 16:37:41 -0400

    Update testing.R

--- examples/testing.R
+++ examples/testing.R
@@ -3,21 +3,39 @@ input_params <- yaml::yaml.load_file(yaml_path)
 
 SLIDE::checkDataParams(input_params)
 
+
+##################################################################
+##                            STEP 1                            ##
+##################################################################
+
+##################################################################
+##                           option 1                           ##
+##################################################################
 ## Approximate the SLIDE model's performance for different delta and lambda. 
 SLIDE::optimizeSLIDE(input_params, sink_file = FALSE)
+
+
+##################################################################
+##                           option 2                           ##
+##################################################################
 
 ## Finding the  SLIDE model's performance  using cross-validation
 SLIDE::SLIDEcv(yaml_path, nrep = 20, k = 5)
 
 ## Depending on the limitations you can run optimizeSLIDE or slidecv
 
+##################################################################
+##                           option 3                           ##
+##################################################################
 
-## You can also run the ```optimizeSLIDE``` and to find an approximate model performance 
-## For an optimum combination of delta and lambda you can run ```SLIDEcv```
+## You can also run the ```optimizeSLIDE``` and to find an approximate optimal model performance 
+## Then you can evaluate the performance of the optimum combination of delta and lambda using ```SLIDEcv```
 
 
+##################################################################
+##                            STEP 2                            ##
+##################################################################
 
-
-## After finding the optimum delta and lambda one you can get correlation network using below command:
+## After finding the optimum delta and lambda one you can get a correlation network using the below command:
 SLIDE::plotCorrelationNetworks(input_params)
 
