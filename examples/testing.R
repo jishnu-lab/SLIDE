1f1895155ef4af87ad8537839e0f7b5b66ecd398
jrahimik <jrahimik1986@gmail.com>
2024-08-12 16:28:40 -0400

    Update testing.R

--- examples/testing.R
+++ examples/testing.R
@@ -2,6 +2,21 @@ yaml_path = "/ix/djishnu/Hanxi/SLIDE/examples/example_binary.yaml"
 input_params <- yaml::yaml.load_file(yaml_path)
 
 SLIDE::checkDataParams(input_params)
+
+## Approximate the SLIDE model's performance for different delta and lambda. 
 SLIDE::optimizeSLIDE(input_params, sink_file = FALSE)
-SLIDE::plotCorrelationNetworks(input_params)
+
+## Finding the  SLIDE model's performance  using cross-validation
 SLIDE::SLIDEcv(yaml_path, nrep = 20, k = 5)
+
+## Depending on the limitations you can run optimizeSLIDE or slidcv
+
+
+## You can also run the ```optimizeSLIDE``` and for a single combination of delta and lambda you can run ```SLIDEcv```
+
+
+
+
+
+SLIDE::plotCorrelationNetworks(input_params)
+
