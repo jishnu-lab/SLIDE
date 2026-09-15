694873797ed21e7a9d459e87237d90056c1122c2
Transcription Factory <transcriptionfactory@gmail.com>
2026-09-15 12:56:52 -0400

    Update SLIDEcv.R
    fixing cv plots so SLIDE box plot is red and SLIDE_y is gray

--- R/SLIDEcv.R
+++ R/SLIDEcv.R
@@ -114,12 +114,12 @@ SLIDEcv <- function(yaml_path=NULL, nrep=20, k=10){
 
 
     if (slide_input$eval_type == "corr") {
-      lambda_boxplot = ggpubr::ggboxplot(data = perRes, x = "method", y = "corr", palette = c("#808180FF", "#EE0000FF"),
+      lambda_boxplot = ggpubr::ggboxplot(data = perRes, x = "method", y = "corr", palette = c("#EE0000FF", "#808180FF"),
                                          fill = "method" ) +
         ggpubr::stat_compare_means(label = "p.signif")
 
     } else {
-      lambda_boxplot = ggpubr::ggboxplot(data = perRes, x = "method", y = "auc", palette = c("#808180FF", "#EE0000FF"),
+      lambda_boxplot = ggpubr::ggboxplot(data = perRes, x = "method", y = "auc", palette = c("#EE0000FF", "#808180FF"),
                                          fill = "method" ) +
         ggpubr::stat_compare_means(label = "p.signif")
     }
